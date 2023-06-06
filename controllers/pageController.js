e3139f15f7bdc54c71cf52aad93f06fbb8f0754e
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-06-06 15:40:56 +0300

    portfolio modal page

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,13 @@
-exports.getHomePage=(req,res)=>{
-    res.render('index')
+const Portfolio = require('./../models/Portfolio')
+
+exports.getHomePage = async (req, res) => {
+    const portfolios = await Portfolio.find()
+    const portfolio = await Portfolio.findById(req.params.id)
+
+    res.render(
+        'index',
+        {
+            portfolios
+        }
+    )
 }
